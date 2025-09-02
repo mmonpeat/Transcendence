fa7fd11a09d292cebc53a39b1fc4a93c25a204a3
Maria Monpeat López <mmonpeat@cbr6s6.42barcelona.com>
2025-09-02 18:42:09 +0200

    executa tot amb npm run all

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,13 @@ console.log("holiii");
 
 // curl -k https://localhost:8000/
 const https = require('node:https');
+//const log = require('pino')({ level: 'info' })
+//const fs = require('fastify')({ logger: log })
+/*
+    "start": "node index.js"
+    "build": "docker build -t node .",
+    "run": "docker run -p 4443:443 node"
+*/
 const fs = require('node:fs');
 
 const options = {
@@ -14,7 +21,10 @@ const options = {
 };
 
 https.createServer(options, (req, res) => {
+  console.log("Request incoming:", req.method, req.url);
   res.writeHead(200);
   console.log("patata");
   res.end('hello world\n');
-}).listen(443);
+}).listen(4443, () => {
+  console.log("server https escolta port 443");
+});
